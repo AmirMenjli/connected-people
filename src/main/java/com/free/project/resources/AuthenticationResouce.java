d4a34b44e6e3dcc81968bf3863133e4a153665b5
amenjli <amir.menjli@advyteam.com>
2018-08-05 01:29:35 +0100

    Add of the @CrossOrigin

--- src/main/java/com/free/project/resources/AuthenticationResouce.java
+++ src/main/java/com/free/project/resources/AuthenticationResouce.java
@@ -3,16 +3,11 @@ package com.free.project.resources;
 import com.free.project.entities.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class AuthenticationResouce {
-
     @RequestMapping("/")
     public String index() {
         return "Connected People!";
